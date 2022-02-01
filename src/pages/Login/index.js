86574b3235750ed46218b4ab2b993c18aad9ba48
Fachreza Maulana <zzafachreza@gmail.com>
2022-02-02 00:17:43 +0700

    alhamdulillah

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
-import {fonts, windowWidth, colors} from '../../utils';
-import {MyInput, MyGap, MyButton} from '../../components';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { fonts, windowWidth, colors } from '../../utils';
+import { MyInput, MyGap, MyButton } from '../../components';
+import axios from 'axios';
+
 
 export default function () {
+
+  const [kirim, setKirim] = useState({});
+
+  const masuk = () => {
+
+    console.log(kirim);
+
+    axios.post('https://simenawan.mpssukorejo.com/api/login.php', kirim).then(res => {
+      console.log(res.data);
+    })
+
+  }
+
   return (
-    <ScrollView style={{padding: 10, flex: 1}}>
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+    <ScrollView style={{ padding: 10, flex: 1 }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Image
           source={require('../../assets/logo.png')}
-          style={{width: 100, height: 100}}
+          style={{ width: 100, height: 100 }}
         />
         <Text
           style={{
@@ -29,16 +44,27 @@ export default function () {
         </Text>
       </View>
       <MyGap jarak={40} />
-      <View style={{padding: 10, marginVertical: 10, flex: 1}}>
-        <MyInput label="NIK" iconname="card" placeholder="Masukan NIK Anda" />
+      <View style={{ padding: 10, marginVertical: 10, flex: 1 }}>
+        <MyInput label="NIK" onChangeText={val => setKirim({
+          ...kirim,
+          nik: val
+        })}
+
+
+          iconname="card" placeholder="Masukan NIK Anda" />
         <MyGap jarak={20} />
         <MyInput
+          onChangeText={val => setKirim({
+            ...kirim,
+            password: val
+          })}
           label="Password"
           iconname="key"
           placeholder="Masukan password Anda"
         />
         <MyGap jarak={40} />
         <MyButton
+          onPress={masuk}
           title="LOGIN SEKARANG"
           warna={colors.primary}
           Icons="log-in-outline"
